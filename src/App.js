5f1d62cae0c872e2b3fa222d3f8d529953b17dfc
ritagouveia22 <73960119+ritagouveia22@users.noreply.github.com>
2021-01-07 22:09:41 +0000

    Added rel="noreferrer"

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       Weather App
-      <footer>This project was coded by Rita and it's <a href="https://github.com/ritagouveia22/weather-app-react/" target="_blank">
+      <footer>This project was coded by Rita and it's <a href="https://github.com/ritagouveia22/weather-app-react/" target="_blank" rel="noreferrer">
 open-sourced on GitHub
       </a> and hosted on Netlify.
       </footer>
