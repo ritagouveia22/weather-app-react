9e8a19d6a1c2494128bf21e78779f7b641eaba60
ritagouveia22 <73960119+ritagouveia22@users.noreply.github.com>
2021-01-07 22:08:00 +0000

    Added footer w/ GitHub link

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App</div>
+      Weather App
+      <footer>This project was coded by Rita and it's <a href="https://github.com/ritagouveia22/weather-app-react/" target="_blank">
+open-sourced on GitHub
+      </a> and hosted on Netlify.
+      </footer>
+      </div>
    
   );
 }
