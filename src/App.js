c9ef0fb4b9eb6988e25f3f0b4739b16d5bb28c32
ritagouveia22 <73960119+ritagouveia22@users.noreply.github.com>
2021-01-07 22:26:12 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>This project was coded by Rita and it's <a href="https://github.com/ritagouveia22/weather-app-react/" target="_blank" rel="noreferrer">
+      <div className="container">
+        <h1>Weather App</h1>
+          <footer>This project was coded by Rita and it's <a href="https://github.com/ritagouveia22/weather-app-react/" target="_blank" rel="noreferrer">
 open-sourced on GitHub
       </a> and hosted on Netlify.
       </footer>
       </div>
+     </div>
    
   );
 }
