5378536e9379d59cdc5e2a4e56f89690b46e83ad
ritagouveia22 <73960119+ritagouveia22@users.noreply.github.com>
2021-01-08 21:35:22 +0000

    Added API call integration with Axios

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Lisbon"/>
         <small>This project was coded by Rita and it's <a href="https://github.com/ritagouveia22/weather-app-react/" target="_blank" rel="noreferrer">
 open-sourced on GitHub
       </a> and hosted on Netlify.
--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 import "./Weather.css"
 
-export default function Weather() {
-    return(
+export default function Weather(props) {
+const [weatherData, setWeatherData] = useState({ready:false})
+
+function handleResponse(response) {
+  setWeatherData({
+    ready: true,
+    city: response.data.name,
+    date: "Thursday Jan 7, 22:00",
+    temperature: Math.round(response.data.main.temp),
+    iconUrl: "https://raw.githubusercontent.com/manifestinteractive/weather-underground-icons/HEAD/dist/icons/black/png/256x256/tstorms.png",
+    description: response.data.weather[0].description,
+    humidity: response.data.main.humidity,
+    wind: response.data.wind.speed
+  })
+}
+
+if (weatherData.ready) {
+return(
     <div className="Weather">
       <form>
         <div className="row">
@@ -17,22 +34,22 @@ export default function Weather() {
       </form>
       <div className="row">
         <div className="col-6">
-          <h1>Lisbon</h1>
-          <p>Last updated at: Thursday Jan 7, 22:00</p>
+          <h1>{weatherData.city}</h1>
+          <p>Last updated at: {weatherData.date}</p>
         </div>
         <div className="col-6">
-          <p><span className="temperature">7</span> <span className="units">ºC | ºF</span></p>
+          <p><span className="temperature">{weatherData.temperature}</span> <span className="units">ºC | ºF</span></p>
         </div>
       </div>
       <div className="row mt-3">
         <div className="col-6">
-          <img src="https://raw.githubusercontent.com/manifestinteractive/weather-underground-icons/HEAD/dist/icons/black/png/256x256/tstorms.png" alt="Clear Sky"/>
+          <img src={weatherData.iconUrl} alt={weatherData.description}/>
         </div>
         <div className="col-6">
           <ul>
-           <li>"Clear Sky"</li>
-           <li>Humidity: 70%</li>
-           <li>Wind: 5 km/h</li>
+           <li className="text-capitalize">"{weatherData.description}"</li>
+           <li>Humidity: {weatherData.humidity}%</li>
+           <li>Wind: {weatherData.wind} km/h</li>
            <li>Sunrise: 07:54</li>
            <li>Sunset: 17:30</li>
           </ul>
@@ -40,4 +57,14 @@ export default function Weather() {
       </div>
     </div>
   )
+} else {
+  const apiKey = "05d59b97163becec12a0f8000856ca3e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`
+  axios.get(apiUrl).then(handleResponse)
+
+  return "Loading..."
+}
+
+  
+    
 }
